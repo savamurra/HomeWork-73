82184a728003460e693dba43f4009b363a03a455
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-27 17:12:40 +0600

    Выполнил первое задание, написал логику ответа exo запроса

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/:text', (req, res) => {
+    res.send(req.params.text);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
